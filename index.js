b53d22f39ca88081b3a69fedb62bb1c7d95d74ee
John Agbaje <86282415+JhainBolar@users.noreply.github.com>
2022-05-18 21:12:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector('.toggle-btn')
+const nav = document.querySelector('.ul')
+
+toggleBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('active')
+    
+})
